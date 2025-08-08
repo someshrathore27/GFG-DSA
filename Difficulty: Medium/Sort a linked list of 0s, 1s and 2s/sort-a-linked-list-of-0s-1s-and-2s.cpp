0a234457880efd338d351017ed2f89ff7f4cdb1e
Somesh Rathore <someshrathore27@gmail.com>
2025-08-08 20:11:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -11,38 +11,43 @@ class Solution {
   public:
     Node* segregate(Node* head) {
         // code here
-        int cnt0=0;
-        int cnt1=0;
-        int cnt2=0;
+        if(head==NULL) return head;
+        Node* p0=new Node(0);
+        Node* p1=new Node(0);
+        Node* p2=new Node(0);
+        Node* p0s=p0;
+        Node* p1s=p1;
+        Node* p2s=p2;
         Node* temp=head;
         while(temp!=NULL)
         {
-            if(temp->data==0)   cnt0++;
-            if(temp->data==1)   cnt1++;
-            if(temp->data==2)   cnt2++;
-            
-            temp=temp->next;
-        }
-        temp=head;
-        while(temp!=NULL)
-        {
-            if(cnt0>0 ) 
+            if(temp->data==0)
             {
-                temp->data=0;
-                cnt0--;
+                p0->next=temp;
+                p0=p0->next;
             }
-            else if(cnt1>0)
+            else if(temp->data==1)
             {
-                temp->data=1;
-                cnt1--;
+                p1->next=temp;
+                p1=p1->next;
             }
-            else if(cnt2>0)
+            else if(temp->data==2)
             {
-                temp->data=2;
-                cnt2--;
+                p2->next=temp;
+                p2=p2->next;
             }
             temp=temp->next;
         }
-        return head;
+        p0->next = p1s->next ? p1s->next : p2s->next;
+        p1->next = p2s->next;
+        p2->next = nullptr; 
+        
+        Node* newHead = p0s->next ? p0s->next : (p1s->next ? p1s->next : p2s->next);
+        
+        delete p0s;
+        delete p1s;
+        delete p2s;
+        
+        return newHead;
     }
 };
