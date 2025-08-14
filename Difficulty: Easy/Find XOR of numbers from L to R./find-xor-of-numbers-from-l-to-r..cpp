843182995ae9fed9d0a8e899fcb0cfcaa7367f29
Somesh Rathore <someshrathore27@gmail.com>
2025-08-14 17:25:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find XOR of numbers from L to R./find-xor-of-numbers-from-l-to-r..cpp
@@ -0,0 +1,29 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int findXOR(int n)
+    {
+        int mod = n % 4;
+    
+        // If n is a multiple of 4
+        if (mod == 0)
+            return n;
+    
+        // If n % 4 gives remainder 1
+        else if (mod == 1)
+            return 1;
+    
+        // If n % 4 gives remainder 2
+        else if (mod == 2)
+            return n + 1;
+    
+        // If n % 4 gives remainder 3
+        else if (mod == 3)
+            return 0;
+    }
+    int findXOR(int l, int r)
+    {
+        return (findXOR(l - 1) ^ findXOR(r));
+    }
+};
